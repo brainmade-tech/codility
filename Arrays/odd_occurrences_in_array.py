fa3a4bec7ed8b730c3318db7d036c400b8875430
YcDev <yassinechaoui95@gmail.com>
2021-07-03 02:26:18 +0100

    Refactor method

--- Arrays/odd_occurrences_in_array.py
+++ Arrays/odd_occurrences_in_array.py
@@ -3,4 +3,6 @@ from typing import Counter
 def solution(A):
     counter = Counter(A)
     A = [key for key in counter if counter[key]%2 == 1]
-    return A[0]
+    if len(A) == 1:
+        return A[0]
+    return -1
